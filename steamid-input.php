1a0cee0667849d1e301b44d4d386eb9ac4dd601a
Nebual <ben1120@gmail.com>
2016-12-04 03:48:46 -0800

    Now supports steamIDs in addition to vanity urls

--- steamid-input.php
+++ steamid-input.php
@@ -2,8 +2,23 @@
 require_once('global.php');
 
 if(!empty($_POST['steamname'])) {
+	$matches = [];
 	$posted_name = $_POST['steamname'];
-	$steam_id = $steam->run(new \Steam\Command\User\ResolveVanityUrl($_POST['steamname']))['response']['steamid'];
+	if(preg_match('/STEAM_([0-9]+):([0-9]+):([0-9]+)/', $posted_name, $matches)) {
+		// Old style SteamID
+		$steam_id = 76561197960265728 + (int)$matches[3] * 2 + (int)$matches[2];
+	} elseif(preg_match('/U:1:([0-9]+)/', $posted_name, $matches)) {
+		// 32 bit steamID3
+		$steam_id = 76561197960265728 + (int)$matches[1];
+	} elseif(preg_match('/(?:(?:https?:\/\/)?steamcommunity\.com\/profiles\/)?([0-9]+)(?:\/.*)?$/', $posted_name, $matches)) {
+		// 64 bit steamID3
+		$posted_name = $matches[1];
+		$steam_id = $matches[1];
+	} elseif(preg_match('/(?:(?:https?:\/\/)?steamcommunity\.com\/id\/)?([A-Za-z0-9_\-]+)(?:\/.*)?$/', $posted_name, $matches)) {
+		// Vanity URL
+		$posted_name = $matches[1];
+		$steam_id = $steam->run(new \Steam\Command\User\ResolveVanityUrl($posted_name))['response']['steamid'];
+	}
 
 	if(!empty($steam_id)) {
 		$_SESSION['steamname'] = $posted_name;
@@ -18,7 +33,7 @@ if(!empty($_POST['steamname'])) {
 
 <form id="steamname-form" ic-post-to="steamid-input.php" ic-indicator="#steamname-loading" ic-replace-target="true" style="max-width: 200px;">
 	<div class="form-group">
-		<label>Steam Vanity URL</label>
+		<label>Steam Vanity URL/SteamID</label>
 		<i id="steamname-loading" class="fa fa-spinner fa-spin" style="display:none"></i>
 		<input type="text" class="form-control" name="steamname" value="<?= $_SESSION['steamname'] ?? ''?>"/>
 	</div>
